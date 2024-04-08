0d0fd99694b8de1dfa0f21597442435edd620adc
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-09 01:40:09 +0300

    edited the login form

--- frontend/src/pages/Auth/login.jsx
+++ frontend/src/pages/Auth/login.jsx
@@ -3,12 +3,12 @@ import "./auth.css";
 function Login() {
   return (
     <div className="page flex center column gap">
-      <div className="login_form flex column align-center ">
+      <div className="register_form flex column align-center ">
         <h1 className="insta flex center">Instagram</h1>
 
-        <input className="login_inputs" placeholder=" Username"></input>
-        <input className="login_inputs" placeholder=" Password"></input>
-        <button className="login_button">Log in</button>
+        <input className="register_inputs" placeholder=" Username"></input>
+        <input className="register_inputs" placeholder=" Password"></input>
+        <button className="register_button">Log in</button>
       </div>
       <div className="redirection flex center gap">
         <p>Don't have an account?</p><button className="switch_button">Sign up</button>
