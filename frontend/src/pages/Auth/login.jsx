d3e7f161190a3063da759370367235bef633df7b
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-09 01:17:28 +0300

    adding login form

--- frontend/src/pages/Auth/login.jsx
+++ frontend/src/pages/Auth/login.jsx
@@ -1,11 +1,20 @@
-import './auth.css';
+import "./auth.css";
 
 function Login() {
-    return <div className="page flex center">
-      <div className="login_form flex column align-center">
-        <h1 className='insta'>Instagram</h1>
+  return (
+    <div className="page flex center column gap">
+      <div className="login_form flex column align-center ">
+        <h1 className="insta flex center">Instagram</h1>
+
+        <input className="login_inputs" placeholder=" Username"></input>
+        <input className="login_inputs" placeholder=" Password"></input>
+        <button className="login_button">Log in</button>
       </div>
-      </div>;
-  }
+      <div className="redirection flex center gap">
+        <p>Don't have an account?</p><button className="switch_button">Sign up</button>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
