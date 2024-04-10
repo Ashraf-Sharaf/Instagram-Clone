2f67b7b76615c91b85afac7af588e634198f5d67
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-10 11:39:58 +0300

    added follow model

--- backend/app/Models/Follower.php
+++ backend/app/Models/Follower.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Follower extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function followed_user(){
+        return $this->belongsTo(User::class);
+    }
 }
