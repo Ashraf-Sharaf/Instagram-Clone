2bae6476edfe72ece5070a86f293ef3bd290e398
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-11 16:15:14 +0300

    edited profile update status

--- frontend/src/pages/Home/profiles/editProfile.jsx
+++ frontend/src/pages/Home/profiles/editProfile.jsx
@@ -10,17 +10,20 @@ function EditProfile() {
         bio: bio,
       };
 
-      const result = await axios.post("http://127.0.0.1:8000/api/updateUser", data, {
-        headers: {
-          Authorization: "Bearer " + window.localStorage.getItem("token"),
-        },
-      });
+      const result = await axios.post(
+        "http://127.0.0.1:8000/api/updateUser",
+        data,
+        {
+          headers: {
+            Authorization: "Bearer " + window.localStorage.getItem("token"),
+          },
+        }
+      );
       if (result.status == 200) {
-        console.log("sssss");
+        alert("Bio updated");
       } else {
         alert("error");
       }
-      setBio = "";
     } catch (error) {
       console.log(error);
     }
@@ -28,13 +31,12 @@ function EditProfile() {
 
   return (
     <div className="flex column edit_container gap">
-      <label>Bio:</label>
+      <label>Edit your bio:</label>
       <input
         className="edit_inputs"
         onChange={(e) => setBio(e.target.value)}
       ></input>
       <div className="flex center">
-        {" "}
         <button className="edit_button" onClick={() => editUserProfile()}>
           Edit
         </button>
