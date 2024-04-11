ecfbb8dd0581fb975296e63ec423059c00333653
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-11 13:34:04 +0300

    added logout function

--- frontend/src/pages/Home/profiles/profile.jsx
+++ frontend/src/pages/Home/profiles/profile.jsx
@@ -5,21 +5,6 @@ import axios from "axios";
 function Profile() {
   const [profile, setProfile] = useState({});
 
-  // function getProfile(){
-  //   axios({
-  //     method: "get",
-  //     url: "http://127.0.0.1:8000/api/getuser",
-  //     headers: {
-  //       Authorization: "Bearer " + window.localStorage.getItem("token"),
-  //     },
-  //   }).then(function(res)
-  //   {
-  //     if (res.status === 200) {
-  //       setProfile(res.data.user);
-  //     }
-  //   });
-  // }
-
   const getProfile = async () => {
     try {
       const result = await axios.get("http://127.0.0.1:8000/api/getuser", {
