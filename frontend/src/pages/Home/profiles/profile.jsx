9ec9340c1578275b84ba19f86c7dd91b4be70da0
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-11 13:19:08 +0300

    edited get user function

--- frontend/src/pages/Home/profiles/profile.jsx
+++ frontend/src/pages/Home/profiles/profile.jsx
@@ -1,30 +1,43 @@
 import "./profile.css";
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Profile() {
   const [profile, setProfile] = useState({});
 
-function getProfile(){
-  axios({
-    method: "get",
-    url: "http://127.0.0.1:8000/api/getuser",
-    headers: {
-      Authorization: "Bearer " + window.localStorage.getItem("token"),
-    },
-  }).then(function(res)
-  {
-    if (res.status === 200) {
-      setProfile(res.data.user);
-    }
-  });
-}
-  
-useEffect(() => {
-  getProfile();
-}, []);
+  // function getProfile(){
+  //   axios({
+  //     method: "get",
+  //     url: "http://127.0.0.1:8000/api/getuser",
+  //     headers: {
+  //       Authorization: "Bearer " + window.localStorage.getItem("token"),
+  //     },
+  //   }).then(function(res)
+  //   {
+  //     if (res.status === 200) {
+  //       setProfile(res.data.user);
+  //     }
+  //   });
+  // }
 
-  const {name,username}=profile;
+  const getProfile = async () => {
+    try {
+      const result = await axios.get("http://127.0.0.1:8000/api/getuser", {
+        headers: {
+          Authorization: "Bearer " + window.localStorage.getItem("token"),
+        },
+      });
+      if (result.status == 200) setProfile(result.data.user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const { name, username } = profile;
 
   return (
     <div className="profiles container flex gap column align-center">
